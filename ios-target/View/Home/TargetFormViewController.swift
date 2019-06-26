d626dfef85d0614ddc34a578b2f8f4d57c86f65a
Sol <sol.manini@rootstrap.com>
2019-06-26 12:57:04 -0300

    Add targetFormViewController to allow the creation of new targets and SaliderAnimationController to handle form animations

---
+++ ios-target/View/Home/TargetFormViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TargetFormViewController.swift
+//  ios-target
+//
+//  Created by sol manini on 6/26/19.
+//  Copyright © 2019 TopTier labs. All rights reserved.
+//
+
+import UIKit
+
+class TargetFormViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func tapSaveButton(_ sender: UIButton) {
+    dismiss(animated: true, completion: nil)
+  }
+}
