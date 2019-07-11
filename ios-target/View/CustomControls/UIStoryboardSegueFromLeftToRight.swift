0751da80292ebf7ec11451a28b4ca4142e1de064
Sol <sol.manini@rootstrap.com>
2019-07-11 11:56:23 -0300

    indentation fix on custom segue

--- ios-target/View/CustomControls/UIStoryboardSegueFromLeftToRight.swift
+++ ios-target/View/CustomControls/UIStoryboardSegueFromLeftToRight.swift
@@ -12,19 +12,26 @@ import UIKit
 class UIStoryboardSegueFromLeftToRight: UIStoryboardSegue {
   
   override func perform() {
+    let animationDuration = 0.35
     let sourceController = source as UIViewController
     let destinationController = destination as UIViewController
-  
+    
     sourceController.view.superview?.insertSubview(destinationController.view,
                                                    aboveSubview: sourceController.view)
-    destinationController.view.transform = CGAffineTransform(translationX: -sourceController.view.frame.size.width, y: 0)
+    destinationController.view.transform = CGAffineTransform(translationX: -sourceController.view.frame.size.width,
+                                                             y: 0)
     
-    UIView.animate(withDuration: 0.35,
+    UIView.animate(withDuration: animationDuration,
+                   delay: 0.0,
+                   options: .curveEaseInOut,
                    animations: {
-                    destinationController.view.transform = CGAffineTransform(translationX: 0, y: 0)
+                    destinationController.view.transform = CGAffineTransform(translationX: 0,
+                                                                             y: 0)
     },
                    completion: { _ in
-                    sourceController.present(destinationController, animated: false, completion: nil)
+                    sourceController.present(destinationController,
+                                             animated: false,
+                                             completion: nil)
     })
   }
 }
@@ -32,19 +39,25 @@ class UIStoryboardSegueFromLeftToRight: UIStoryboardSegue {
 class UIStoryboardUnwindSegueFromLeftToRight: UIStoryboardSegue {
   
   override func perform() {
+    let animationDuration = 0.35
     let sourceController = source as UIViewController
     let destinationController = destination as UIViewController
     
     sourceController.view.superview?.insertSubview(destinationController.view,
                                                    belowSubview: sourceController.view)
-    sourceController.view.transform = CGAffineTransform(translationX: 0, y: 0)
+    sourceController.view.transform = CGAffineTransform(translationX: 0,
+                                                        y: 0)
     
-    UIView.animate(withDuration: 0.35,
+    UIView.animate(withDuration: animationDuration,
+                   delay: 0.0,
+                   options: .curveEaseInOut,
                    animations: {
-                    sourceController.view.transform = CGAffineTransform(translationX: -sourceController.view.frame.size.width, y: 0)
+                    sourceController.view.transform = CGAffineTransform(translationX: -sourceController.view.frame.size.width,
+                                                                        y: 0)
     },
                    completion: { _ in
-                    sourceController.dismiss(animated: false, completion: nil)
+                    sourceController.dismiss(animated: false,
+                                             completion: nil)
     })
   }
 }
